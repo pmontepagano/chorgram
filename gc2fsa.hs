965897122bf76d213b502f9cc8fb9f4f9220f351
Pablo Montepagano <pablo.montepagano@dialpad.com>
2023-05-07 23:54:57 -0300

    Split up the function from gc2fsa to be able to export a function that simply has this signature: String -> String.
    This is so that we can export that function as a WASI target and use it from our Go code in the middleware.

--- gc2fsa.hs
+++ gc2fsa.hs
@@ -24,25 +24,33 @@ main = do progargs <- getArgs
               let ( sourcefile, flags ) =
                     getCmd GC2FSA progargs
               gctxt <- readFile sourcefile
-              let ( gc, names ) =
-                    case gcgrammar gctxt (0, 0) (0, 0) of
-                      Ok x -> x
-                      Er err -> error err
-              let ptps =
-                    Data.Set.toList names
-              -- TODO: fix this String -> [CFSM] -> [[String]] -> System inefficiency
-              let ptp_map =
-                    M.fromList $ L.zip (range $ L.length ptps) ptps
-              let loops =
-                    (read (flags!"-u"))::Int
-              let cfsms =
-                    L.map (minimise . fst) (L.map (\p -> projx False gc ptp_map p "q0" "qe" 1) ptps)
-              let fsa = L.map (\(p, m) -> (CFSM.cfsm2String p m) ++ "\n\n") (L.zip ptps cfsms)
-              if ("" == flags!"-o")
-                then putStrLn $ L.concat fsa
-                else do
-                     mapM_
-                       (\(p,m) ->
-                          writeToFile ((flags!"-o") ++ (if p=="" then "" else "_cfsm_" ++ p) ++ ".fsa") m)
-                       ([("", L.concat fsa)] ++ (L.zip ptps fsa))
+              maingc2fsa (gc2fsafunc gctxt) flags
 
+gc2fsafunc gctxt = do
+      let ( gc, names ) =
+            case gcgrammar gctxt (0, 0) (0, 0) of
+            Ok x -> x
+            Er err -> error err
+      let ptps =
+            Data.Set.toList names
+      -- TODO: fix this String -> [CFSM] -> [[String]] -> System inefficiency
+      let ptp_map =
+            M.fromList $ L.zip (range $ L.length ptps) ptps
+      -- let loops =
+      --       (read (flags!"-u"))::Int
+      let cfsms =
+            L.map (minimise . fst) (L.map (\p -> projx False gc ptp_map p "q0" "qe" 1) ptps)
+      let fsa = L.map (\(p, m) -> (CFSM.cfsm2String p m) ++ "\n\n") (L.zip ptps cfsms)
+      (fsa, ptps)
+
+maingc2fsa (gcfsaoutput, names) flags = do
+      if ("" == flags!"-o")
+      then putStrLn $ L.concat gcfsaoutput
+      else do
+            mapM_
+                  (\(p,m) ->
+                        writeToFile ((flags!"-o") ++ (if p=="" then "" else "_cfsm_" ++ p) ++ ".fsa") m)
+                  ([("", L.concat gcfsaoutput)] ++ (L.zip names gcfsaoutput))
+
+foreign export ccall gc2fsaexportable :: CString -> CString
+gc2fsaexportable x = L.concat (fst (gc2fsafunc x))
